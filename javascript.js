06589d661c6c59681415eb87f41c7a4cb2635f01
Doverspike <derondoverspike@gmail.com>
2024-04-17 20:34:56 -0400

    Functionality for Random and Standard colorings

--- javascript.js
+++ javascript.js
@@ -4,16 +4,31 @@ const div64 = document.getElementById("64");
 const div100 = document.getElementById("100");
 const gridBox = document.getElementById("gridBox");
 const sketchBox = document.getElementById("sketchBox");
-let colorChoice = "#000000";
+const userChoice = document.getElementById("choice");
+const rColor = document.getElementById("randomColor");
+const sColor = document.getElementById("standardColor");
+let colorChoice = "";
 
+rColor.addEventListener("click", () => {
+    colorChoice = "random";
+});
+sColor.addEventListener("click", () => {
+    colorChoice = "standard";
+})
 
+function randomColor() {
+    let color = '#' + Math.floor(Math.random()*16777215).toString(16);
+    return color;
+}
 
+// Creates a grid of DIVs with mouseover background color
 function createDiv(divSize, color) {
     let size = divSize*divSize;
     let oldSize = divSize;
     let boxWidth = getWidth(oldSize);
     let boxHeight = getHeight(oldSize);
     let oldDiv = document.getElementsByClassName("gridItem");
+    
     while (oldDiv.length > 0) {
         oldDiv[0].parentNode.removeChild(oldDiv[0]);
     }
@@ -25,13 +40,20 @@ function createDiv(divSize, color) {
         
         div.addEventListener("mouseover", () => (
             backgroundColor(div)));
+            
+            
+
         
-        gridBox.appendChild(div);
+        sketchBox.appendChild(div);
     }
 };
 
 function backgroundColor(div) {
-    div.style.backgroundColor = colorChoice;
+    if (colorChoice == "random") {
+    div.style.backgroundColor = randomColor();
+    } else {
+        div.style.backgroundColor = "black";
+    }
 }
 
 function getWidth(sizeInput) {
@@ -40,10 +62,8 @@ function getWidth(sizeInput) {
 function getHeight(sizeInput) {
     return (600 / sizeInput) + "px"
 };
-//function hoverColor(color) {
-//   let boxColor = color;
-//   div.style.backgroundColor = boxColor;
-//}
+
+
 
 
 div16.addEventListener("click", () => {
@@ -58,3 +78,14 @@ div64.addEventListener("click", () => {
 div100.addEventListener("click", () => {
     createDiv(100, colorChoice);
 });
+
+userChoice.addEventListener("click", () => {
+    let uChoice = prompt("Input number between 1->100");
+    while (isNaN(uChoice) == true) {
+       uChoice = prompt("Not Valid. Input number between 1->100");
+    } if (uChoice > 100) {
+            uChoice = 100;
+        }
+        createDiv(uChoice, colorChoice);
+    }
+);
